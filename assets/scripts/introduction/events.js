a832c305639659a51de59f39cdec221d8586b303
moiherrera <moi.herrera@outlook.com>
2019-10-17 11:34:38 -0400

    No change made, error

--- assets/scripts/introduction/events.js
+++ assets/scripts/introduction/events.js
@@ -1,13 +1,16 @@
 'use strict'
 import $ from 'jquery'
-// const flyerEvents = require('./../flyer/events')
+const flyerEvents = require('./../flyer/events')
+// const store = require('./../store')
+
 const onClickFirst = function () {
   $('.message-container').hide()
   $('.upper-bar').show()
   $('.change-password').hide()
   $('.sign-out').hide()
   $('.upload-flyer').hide()
-  // flyerEvents.onGetFlyers(event)
+  flyerEvents.onGetFlyers(event)
+  $('.message-box').text('Welcome to Flyer. Enjoy browsing through local happenings.')
 }
 
 const addIntroHandlers = function () {
